a9edb7b012657dc019e53f7f827f1f3975dcc044
Daniel Meagher <dmeagher101@gmail.com>
2017-05-27 19:00:39 -0500

    Added initial files

---
+++ part3/src/TetroType.elm
@@ -0,0 +1,34 @@
+module TetroType exposing (..)
+
+import Array exposing (..)
+import Board exposing (..)
+
+type alias TetroType = Array (Int, Int)
+
+l_type : TetroType
+l_type =
+  fromList [(0,0), (0,1), (0,2), (1,2)]
+
+t_type : TetroType
+t_type =
+  fromList [(0, 0), (1, 0), (2, 0), (1, 1)]
+
+i_type : TetroType
+i_type =
+  fromList [(0,0), (1,0), (2, 0), (3, 0)]
+
+o_type : TetroType
+o_type =
+  fromList [(0,0), (1,0), (0,1), (1,1)]
+
+s_type : TetroType
+s_type =
+  fromList [(1,0), (2,0), (0,1), (1,1)]
+
+j_type : TetroType
+j_type =
+  fromList [(1,0), (1,1), (0,2), (1,2)]
+
+z_type : TetroType
+z_type =
+  fromList [(0,0), (1,0), (0,1), (1,1)]
